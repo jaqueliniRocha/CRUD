062ec2b15424d8c872269e7cff24b9dff01ed267
jaquelini rocha <jaquelini.rocha@gmail.com>
2018-04-16 20:49:57 -0300

    Adicionada biblioteca lombok

--- src/main/java/com/meunuts/pdv/model/Cliente.java
+++ src/main/java/com/meunuts/pdv/model/Cliente.java
@@ -1,71 +1,15 @@
 package com.meunuts.pdv.model;
 
+import lombok.Data;
+
+@Data
 public class Cliente {
 
 	private String id;
-
 	private String nome;
-
 	private String cpf;
-
 	private String endereco;
-
 	private String celular;
-
 	private String email;
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getCpf() {
-		return cpf;
-	}
-
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
-
-	public String getEndereco() {
-		return endereco;
-	}
-
-	public void setEndereco(String endereco) {
-		this.endereco = endereco;
-	}
-
-	public String getCelular() {
-		return celular;
-	}
-
-	public void setCelular(String celular) {
-		this.celular = celular;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	@Override
-	public String toString() {
-		return "Cliente [id=" + id + ", nome=" + nome + ", cpf=" + cpf + ", endereco=" + endereco + ", celular="
-				+ celular + ", email=" + email + "]";
-	}
 
 }
